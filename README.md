078329604a425519269827cdf464312761f864b9
neilrichter <me@neilrichter.com>
2019-01-21 22:13:51 +0100

    ✨ Added response time calculation

--- README.md
+++ README.md
@@ -4,4 +4,18 @@
 
 Create a virtual environment with `python3 -m venv venv`, then source it with `source venv/bin/activate`.
 
+Run `pip install -r requirements.txt` to install the required dependencies.
+
 Code :wink:
+
+## Running the script
+
+Run `./main.py [urls]` or `python3 main.py [urls]` with at least one URL and let the script run.
+
+Example: 
+```sh
+$ ./main.py https://nook.sh https://google.com
+
+> 2019-01-21 22:08:26 [https://nook.sh] Response time : 23.51 ms
+> 2019-01-21 22:08:26 [https://google.com] Response time : 18.056 ms
+```
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import argparse
-import requests
-import json
+import math, argparse, requests, json, time, coloredlogs, logging
+
+logger = logging.getLogger(__name__)
+coloredlogs.DEFAULT_LOG_FORMAT = "%(asctime)s %(message)s"
+coloredlogs.install(logger=logger)
 
 
 def parse_args():
@@ -13,13 +15,23 @@ def parse_args():
             list of flags
         """
     )
-    parser.add_argument("url", help="URL on which the tests will be made.")
+    parser.add_argument("urls", nargs="+", help="URLs on which the tests will be made.")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    print(args.url)
+    for url in args.urls:
+        # Put tests functions here
+        request_time(url)
+
+
+def request_time(url):
+    begin = time.process_time_ns()
+    requests.get(url)
+    end = time.process_time_ns()
+    difference = (end - begin) / (1 * math.pow(10, 6))  #  Convert ns to ms
+    logger.info(f"[{url}] Response time : {difference} ms")
 
 
 if __name__ == "__main__":
